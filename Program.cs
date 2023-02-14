061b017ae5e16d4b5f29b32253387e2309a7cf4f
Brianna Renni <briannaxrene@icloud.com>
2023-02-14 14:09:22 -0800

    Refactor POST/PATCH with [FromBody]

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using TicketAPI_Data;
 using TicketAPI_Models;
 
@@ -29,9 +30,19 @@ if (app.Environment.IsDevelopment())
 
 app.UseCors();
 
-app.MapPost("/users/login", (UserRepo uRepo, string username, string password) => uRepo.validateLogin(username, password));
+app.MapPost("/users/login", (UserRepo uRepo, [FromBody] UserCreds loginData) =>
+{
+    string username = loginData.Username;
+    string password = loginData.Password;
+    return uRepo.validateLogin(username, password);
+});
 
-app.MapPost("/users/register", (UserRepo uRepo, string username, string password) => uRepo.validateRegister(username, password));
+app.MapPost("/users/register", (UserRepo uRepo, [FromBody] UserCreds regData) =>
+{
+    string username = regData.Username;
+    string password = regData.Password;
+    return uRepo.validateRegister(username, password);
+});
 
 app.MapGet("/users/{id}", (UserRepo uRepo, int userId) => uRepo.getUserInfo(userId));
 
@@ -39,9 +50,9 @@ app.MapGet("users/employees", (UserRepo uRepo) => uRepo.getEmployees());
 
 app.MapPatch("/users/{id}/role", (UserRepo uRepo, int userId) => uRepo.changeRole(userId));
 
-app.MapPatch("/users/{id}/password", (UserRepo uRepo, string username, string pw1, string pw2) =>
+app.MapPatch("/users/{id}/password", (UserRepo uRepo, [FromBody] PasswordUpdate data) =>
 {
-    uRepo.updatePassword(username, pw1, pw2);
+    uRepo.updatePassword(data.Username, data.Password, data.ConfirmPassword);
 });
 
 app.MapPost("/users/{id}/tickets", (TicketRepo tRepo, int userId) =>
@@ -50,9 +61,9 @@ app.MapPost("/users/{id}/tickets", (TicketRepo tRepo, int userId) =>
     return (response.Count >= 1) ? Results.Ok(response) : Results.NotFound("Error: No tickets found. Please check that the user ID is valid.");
 });
 
-app.MapPost("/tickets/submit", (TicketRepo tRepo, int userId, string username, double amount, string category, string comments) =>
+app.MapPost("/tickets/submit", (TicketRepo tRepo, [FromBody] NewTicket data) =>
 {
-    Ticket newTicket = new Ticket(userId, username, amount, category, comments);
+    Ticket newTicket = new Ticket(data.UserId, data.Username, data.Amount, data.Category, data.Comments);
     tRepo.addTicket(newTicket);
 });
 
@@ -69,3 +80,7 @@ app.MapPost("/tickets/{id}", (TicketRepo tRepo, int ticketId) =>
 app.MapPatch("/tickets/{id}", (TicketRepo tRepo, int ticketId, string status) => tRepo.updateTicketStatus(ticketId, status));
 
 app.Run();
+
+public record UserCreds(string Username, string Password);
+public record PasswordUpdate(string Username, string Password, string ConfirmPassword);
+public record NewTicket(int UserId, string Username, double Amount, string Category, string Comments);
